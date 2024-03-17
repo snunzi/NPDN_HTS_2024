a1b22ffb544a3b69a6cb2926fc32e8fce652e1b8
snunzi <schylernunziata@gmail.com>
2024-03-17 13:49:27 -0400

    add requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,30 +17,38 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'NPDN_HTS_2024'
-copyright = '2024, Schyler Nunziata'
-author = 'Schyler Nunziata'
-
-# The full version, including alpha/beta/rc tags
-release = '0.0.1'
+project = "NPDN 2024"
+copyright = "2022, Read the Docs core team"
+author = "Read the Docs core team"
 
 
 # -- General configuration ---------------------------------------------------
+# -- General configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -52,19 +60,19 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
 
-# SLIDES APP
-#import os, sys
-
-# Path to the folder where blockdiag.py is
-# NOTE: not needed if the package is installed in traditional way
-# using setup.py or easy_install
-#sys.path.append(os.path.abspath('/path/to/sphinxcontrib.slide'))
-
-# Enabled extensions
-extensions = ['sphinx_rtd_theme','sphinxcontrib.slide']
-
-def setup(app):
-    app.add_css_file('custom.css')
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
